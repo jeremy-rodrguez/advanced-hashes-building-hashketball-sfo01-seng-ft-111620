ce675fa7b72a80347ff86ade785cad4ea24ff2b7
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-11-06 03:24:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -99,15 +99,15 @@ def game_hash
         :slam_dunks => 5
       },
       {
-        :player_name => "Alan Anderson",
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
+        :player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
         :blocks => 1,
-        :slam_dunks => 1
+        :slam_dunks => 0
       },
       {
         :player_name => "Alan Anderson",
